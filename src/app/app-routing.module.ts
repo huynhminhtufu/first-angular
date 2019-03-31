6d5a9aabdaa7ddfafde41c76476a261684460ec4
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-03-31 14:58:46 +0700

    update linter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { Lab11Component } from "./lab11/lab11.component";
+
+const routes: Routes = [
+  {
+    path: "/lab11",
+    component: Lab11Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
